57940d8e29a2bc541cae8c2965e7293bb52e6388
Vicent <vicent@softoursistemas.com>
2026-02-05 11:52:26 +0100

    Implementada la llamada para obtener el listado de pokemons

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -10,3 +10,24 @@
 
   <RouterView />  
 </template>
+
+<script setup lang="ts">
+import { PokemonService } from '@/services/pokemon.service';
+import { usePokemonStore } from '@/store';
+import { onMounted } from 'vue';
+
+const store = usePokemonStore()
+
+const initPokemons = 0;
+const limitPokemons = 151;
+
+onMounted(async () => {
+  try {
+    const pokemons = await PokemonService.getPokemonList(initPokemons, limitPokemons);
+    store.pokemons = pokemons.results;
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+</script>
