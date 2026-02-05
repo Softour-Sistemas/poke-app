8d1caaca4078abd92e5bcc366d1b274cd44c5366
sandra@softoursistemas.com <sandra@softoursistemas.com>
2026-02-05 12:35:45 +0100

    restructuracion de los componentes en carpetas

---
+++ src/components/PokemonGrid/PokemonGrid.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="pokemon-grid">
+        <Pokemon v-for="pokemon in pokemons" :key="pokemon.name" :pokemon="pokemon" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Pokemon } from '@/models/pokemon';
+
+defineProps<{
+    pokemons: Pokemon[];
+}>();
+</script>
+
+
+<style scoped lang="scss">
+@import "./PokemonGrid.scss";
+</style>
